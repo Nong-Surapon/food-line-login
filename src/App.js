5c95ccd248cd59c3104aaa71923611ec8f4b84e0
Surapon Thongnen <modernlife1988@gmail.com>
2021-07-25 20:02:56 +0700

    update main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import liff from '@line/liff';
 
 function App() {
+  const [pictureUrl, setPictureUrl] = useState(
+    'https://profile.line-scdn.net/0hp4UmLn1zLxtoFAaAdIxQTFRRIXYfOilTEHc0Lh8UeHlNcWkfAyVmdUsTcyNCI2odB3MzeERBJCpC/preview'
+  );
+  const [idToken, setIdToken] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
+  const [userId, setuserId] = useState('');
+
+  const logout = () => {
+    console.log('logout');
+    liff.logout();
+    window.location.reload();
+  };
+
+  const getLine = () => {
+    liff
+      .init({
+        liffId: '1655207232-0MxWBN5K',
+      })
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          runApp();
+        }
+      })
+      .catch((err) => {
+        document.getElementById('liffAppContent').classList.add('hidden');
+        document
+          .getElementById('liffInitErrorMessage')
+          .classList.remove('hidden');
+      });
+  };
+
+  const runApp = () => {
+    const idToken = liff.getIDToken();
+    setIdToken(idToken);
+    liff
+      .getProfile()
+      .then((profile) => {
+        setDisplayName(profile.displayName);
+        setPictureUrl(profile.pictureUrl);
+        setStatusMessage(profile.statusMessage);
+        setuserId(profile.userId);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getLine();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>Line Login</h1>
+        <img src={pictureUrl} alt="Logo" />
+        <h2>{idToken}</h2>
+        <h2>{displayName}</h2>
+        <h2>{statusMessage}</h2>
+        <h2>{userId}</h2>
+        <botton onClick={() => logout()} type="primary">
+          Logout
+        </botton>
+      </>
     </div>
   );
 }
